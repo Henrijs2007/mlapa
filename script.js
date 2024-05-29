aebf3a544f8bfafd7fbf772f983897f6ac646581
HenryFrom2007 <henrijs008@gmail.com>
2024-05-29 03:16:53 +0300

    etet

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    AOS.init({
-        duration: 1000, // animācijas ilgums milisekundēs
-        once: true // animācijas izpilda tikai vienreiz
-    });
-
-    // Header animācija pie lapas ielādes
     const header = document.getElementById('main-header');
-    window.addEventListener('load', function() {
-        header.classList.add('loaded');
-    });
+    header.classList.add('loaded');
 
-    // Header samazināšanās efekts, kad lapa tiek scrollēta
     window.addEventListener('scroll', function() {
         if (window.scrollY > 50) {
             header.classList.add('small');
@@ -18,4 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
             header.classList.remove('small');
         }
     });
+
+    AOS.init();
 });
